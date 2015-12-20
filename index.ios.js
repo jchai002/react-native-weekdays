37825d8a307b4cd5f8e5ae35fa903d1bd0d200b3
Jerry Chai <jchai002@gmail.com>
2015-12-20 11:10:57 -0800

    first commit

---
+++ index.ios.js
@@ -0,0 +1,45 @@
+//import code we need
+var Moment = require('moment');
+var React=require('react-native');
+var AppRegistry= React.AppRegistry;
+var Text=React.Text;
+var View=React.View;
+var StyleSheet = React.StyleSheet
+var DayItem=require('./src/day-item');
+var DAYS=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+// Create a react component
+var Weekdays = React.createClass({
+  render: function(){
+    return <View style={styles.container}>
+      {this.days()}
+    </View>
+  },
+  days: function(){
+
+    var daysItems=[]
+
+    for (var i =0;i<7;i++){
+      var day = Moment().add(i,'days').format('dddd');
+      daysItems.push(
+        <DayItem day={day} daysUntil={i} />
+      )
+    }
+
+    return daysItems
+  }
+})
+
+//style the component
+
+var styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+  }
+});
+
+//show component on screen
+AppRegistry.registerComponent('weekdays',function(){
+  return Weekdays
+})
